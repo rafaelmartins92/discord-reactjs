5d4a06581c7f54ed236a6c0208330d37877e6a8c
Rafael Martins <rafael.martins92@outlook.com>
2020-07-11 23:06:46 -0300

    Add GlobalStyle

---
+++ src/styles/global.js
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    outline: 0;
+
+    font-family: 'Roboto', sans-serif;
+  }
+
+  :root {
+    --primary: #36393f;
+    --secondary: #2f3136;
+    --tertiary: rgb(32,34,37);
+    --quarternary: #292b2f;
+    --quinary: #393d42;
+    --senary: #828386;
+
+    --white: #fff;
+    --gray: #8a8c90;
+    --chat-input: rgb(64,68,75);
+    --symbol: #74777a;
+
+    --notification: #f84a4b;
+    --discord: #6e86d6;
+    --mention-detail: #f9a839;
+    --mention-message: #413f3f;
+
+    --link: #5d80d6;
+
+    --rocketseat: #6633cc;
+  }
+
+`
+
+export default GlobalStyle
