4b656a52376f16ce25038b9089e56a933bc4483b
Rafael Martins <rafael.martins92@outlook.com>
2020-07-12 21:08:42 -0300

    Create ServerName

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { Grid } from './styles';
 
 import ServerList from '../ServerList'
+import ServerName from '../ServerName'
+// import ChannelInfo from '../ChannelInfo'
 
 function Layout() {
   return (
     <Grid>
       <ServerList />
+      <ServerName />
     </Grid>
   )
 }
---
+++ src/components/ServerName/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Title, ExpandIcon } from './styles';
+
+function ServerName() {
+  return (
+    <Container>
+      <Title>Servidor do Raafs</Title>
+
+      <ExpandIcon />
+    </Container>
+  )
+}
+
+export default ServerName;
