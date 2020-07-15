c90f5a2460c069187443fd6de7be7a58b5288716
Rafael Martins <rafael.martins92@outlook.com>
2020-07-15 11:24:58 -0300

    Create ChannelMessage

--- src/components/ChannelData/index.js
+++ src/components/ChannelData/index.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+
+import ChannelMessage, { Mention } from "../ChannelMessage";
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from "./styles";
 
 function ChannelData() {
+  const messagesRef = useRef(null);
+
+  useEffect(() => {
+    messagesRef.current.scrollIntoView({ behavior: "smooth" });
+  });
+
   return (
     <Container>
-      <Messages />
+      <Messages>
+        {Array.from(Array(15).keys()).map((n) => (
+          <ChannelMessage
+            author="Rafael Martins"
+            date="14/07/202"
+            content="Eae galera!!!"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Guilherme Rodz"
+          date="14/07/202"
+          content={
+            <>
+              <Mention>@Rafael Martins</Mention>, gostou do tutorial?
+            </>
+          }
+          hasMention
+          isBot
+          ref={messagesRef}
+        />
+
+        <div ref={messagesRef} />
+      </Messages>
 
       <InputWrapper>
         <Input type="text" placeholder="Conversar em # 💬| bate-papo" />
